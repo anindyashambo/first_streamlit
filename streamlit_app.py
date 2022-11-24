3bc4a94b0511564bc3a8c4c9d377de7704c30c73
anindyashambo <94078658+anindyashambo@users.noreply.github.com>
2022-11-24 14:59:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,6 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 streamlit.dataframe(my_fruit_list)  
 
 streamlit.multiselect(" pick me some fruits :" , list(my_fruit_list.index))
-streamlit.dataframe(my_fruit_list)
-  
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.dataframe(my_fruit_list)  
