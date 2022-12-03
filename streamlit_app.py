19dfa284dc7258719ab375bdcb86e958978f5e8c
anindyashambo <94078658+anindyashambo@users.noreply.github.com>
2022-12-03 10:07:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit 
+import request
 streamlit.title("My parents new healthy diner")
 
 streamlit.header('Breakfast Menu')
@@ -13,6 +14,6 @@ streamlit.dataframe(my_fruit_list)
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(fruits_to_show)  
 
-import request
+
 fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
