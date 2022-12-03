27bec9149a1092708c6138adbeb96d3ce45ec63b
anindyashambo <94078658+anindyashambo@users.noreply.github.com>
2022-12-03 11:00:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,10 @@ streamlit.dataframe(my_fruit_list)
 #streamlit.dataframe(fruits_to_show)  
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + "kiwi")
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
