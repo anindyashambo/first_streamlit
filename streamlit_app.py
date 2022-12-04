2e021d5031021d109571df58451fc19dc150a2e2
anindyashambo <94078658+anindyashambo@users.noreply.github.com>
2022-12-04 14:26:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,15 +24,28 @@ streamlit.dataframe(my_fruit_list)
 #streamlit.text(fruityvice_response.json())
 
 
+streamlit.header('Fruityvice fruit advice')
+try:
+  
+  
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Lime')
+  if not fruit_choice:
+    streamlit.error("please select to get fruit info")
+  else:
+    
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+ except URLError as e:
+  streamlit.error()
+    
+  #streamlit.write('The user entered ', fruit_choice)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Lime')
-streamlit.write('The user entered ', fruit_choice)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+#streamlit.dataframe(fruityvice_normalized)
 
 # write your own comment -what does the next line do? 
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
